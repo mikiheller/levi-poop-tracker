fb6976ee16d4560e084895a7c254bdabfd16c657
Instinct <instinct@users.noreply.github.com>
2026-09-03 18:44:11 -0700

    Add Sep 3 entry

--- poop_data.js
+++ poop_data.js
@@ -1,5 +1,5 @@
 const POOP_META = {
-  "generated_at": "2026-09-03T02:54:18Z"
+  "generated_at": "2026-09-04T01:44:11Z"
 };
 const POOP_ENTRIES = [
   {
@@ -1404,6 +1404,15 @@ const POOP_ENTRIES = [
     "texture": "mashed potatoes",
     "notes": "Pooped at 7:03pm; big, mashed potatoes (potty-training channel in Slack)",
     "logged": "2026-09-03T02:14:21Z",
+    "by": "Miki"
+  },
+  {
+    "date": "2026-09-03",
+    "time": "18:27",
+    "size": "big",
+    "texture": "mashed potatoes",
+    "notes": "Suggested he use the toilet at 6:27pm; peed standing, then sat and pooped; big amount, mashed potatoes (forwarded from potty-training channel in Slack)",
+    "logged": "2026-09-04T01:32:17Z",
     "by": "Miki"
   }
 ];
